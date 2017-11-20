15ac6a8232a58b4bbec108f544e40d5bcf7dd788
Whitney <whitbyte@gmail.com>
2017-11-20 19:19:00 +0000

    user statuses returned

---
+++ user_request.py
@@ -0,0 +1,11 @@
+import twitter, os, requests
+
+api = twitter.Api(
+    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+    access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+statuses = api.GetUserTimeline(screen_name='whitbit_')
+
+print [s.text for s in statuses]
